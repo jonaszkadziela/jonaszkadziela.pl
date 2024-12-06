0a967544941ab1c0d3c78c33e618db5dbce6a002
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-12-06 20:59:43 +0100

    Adjust code style in js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -17,4 +17,4 @@ export default {
         },
     },
     plugins: [],
-};
+}
