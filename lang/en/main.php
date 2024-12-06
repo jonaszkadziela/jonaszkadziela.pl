2c6fcd85dc57acf58c9536bb7ea910e1b05dcdcc
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-12-06 21:44:39 +0100

    Add Main layout

---
+++ lang/en/main.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Main Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the main layout.
+    |
+    */
+
+    'description' => 'Personal website of Jonasz Kądziela - See my portfolio and get to know me',
+
+];
---
+++ lang/pl/main.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Main Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the main layout.
+    |
+    */
+
+    'description' => 'Strona internetowa Jonasza Kądzieli - Zobacz moje portfolio i poznaj mnie',
+
+];
