7f1f3b70faaee663c29ddc475ccee6a352010c11
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-12-06 20:59:26 +0100

    Add PrimeVue configuration

---
+++ resources/js/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="text-xl">
+        Jonasz Kądziela
+    </h1>
+</template>
+
+<script setup>
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import './bootstrap'
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+app.mount('#app')
