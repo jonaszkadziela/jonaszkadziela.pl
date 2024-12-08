065ecb75f86e4ad07835226cad1de5e82e857adc
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-12-08 21:44:15 +0100

    Add brand images and FontAwesome icons

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,10 @@
         <meta property="og:site_name" content="{{ config('app.name') }}">
         <meta property="og:title" content="{{ (isset($title) ? $title . ' - ' : '') . config('app.name') }}">
         <meta property="og:description" content="{{ Lang::get('main.description') }}">
+        <meta property="og:image" content="{{ Vite::asset('resources/images/brand/og-image.jpg') }}">
         <meta property="og:url" content="{{ config('app.url') }}">
+        <link href="{{ Vite::asset('resources/images/brand/favicon.png') }}" rel="shortcut icon" type="image/png">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" rel="stylesheet" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="{{ isset($bodyClass) ? $bodyClass : '' }}">
