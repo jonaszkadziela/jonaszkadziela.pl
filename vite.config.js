7304fa092e5867d642836d36ef0ef61ce21b84c7
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-12-08 21:44:37 +0100

    Add @ alias to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import Components from 'unplugin-vue-components/vite'
 import laravel from 'laravel-vite-plugin'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    resolve: {
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, './resources') },
+        ],
+    },
     plugins: [
         laravel({
             input: [
