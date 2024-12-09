b2897251bec8b0347dbfca05dbc4a4091b684aeb
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-12-09 22:58:00 +0100

    Add first version of NavBar component

--- resources/js/components/layouts/DefaultLayout.vue
+++ resources/js/components/layouts/DefaultLayout.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="min-h-screen">
+        <NavBar />
         <main class="container mx-auto py-2">
             <Button label="Toggle dark mode" class="mb-4 mt-2" @click="toggleDarkMode" />
             <slot />
@@ -8,6 +9,8 @@
 </template>
 
 <script setup>
+import NavBar from '../shared/NavBar.vue'
+
 const html = document.getElementsByTagName('html')[0]
 const toggleDarkMode = () => html.classList.contains('dark') ? html.classList.replace('dark', 'light') : html.classList.replace('light', 'dark')
 </script>
---
+++ resources/js/components/shared/NavBar.vue
@@ -0,0 +1,102 @@
+<template>
+    <nav :class="navBarSticky ? 'backdrop-blur-md bg-white/75 dark:bg-black/75 dark:shadow-slate-800 shadow-lg transition-shadow' : 'dark:bg-black bg-white'"
+         class="sticky top-[-1px] z-50"
+         ref="navBar"
+    >
+        <Menubar :model="items"
+                 :pt="{
+                     button: 'flex md:hidden ml-auto p-2',
+                     itemContent: 'container md:m-0 mx-auto my-1',
+                     itemLabel: ({ context }) => [{
+                        'dark:text-gray-300 hover:underline text-gray-600 underline-offset-8': true,
+                        'font-bold': context.index === 0,
+                        'underline': context.focused,
+                     }],
+                     root: 'container delay-100 duration-300 flex gap-4 items-center mx-auto py-4',
+                     rootList: ({ props }) => [{
+                        'gap-6 md:flex md:mr-2 md:py-2 ml-auto': true,
+                        'absolute border-t dark:border-gray-600 left-0 p-4 right-0 top-full': props.mobileActive,
+                        'hidden': !props.mobileActive,
+                        'bg-white/90 dark:bg-black/90': props.mobileActive && navBarSticky,
+                        'dark:bg-black bg-white': props.mobileActive && !navBarSticky,
+                     }],
+                 }"
+                 breakpoint="768px"
+                 unstyled
+        >
+            <template #start>
+                <a href="/"
+                   class="flex items-center"
+                >
+                    <img :src="logo"
+                         alt="Logo"
+                         class="w-12 mr-2"
+                    >
+                    <span class="font-semibold">
+                        Jonasz Kądziela
+                    </span>
+                </a>
+            </template>
+        </Menubar>
+    </nav>
+</template>
+
+<script setup>
+import LogoBlack from '@/images/brand/logo-black.svg'
+import LogoWhite from '@/images/brand/logo-white.svg'
+import {
+    computed,
+    onBeforeUnmount,
+    onMounted,
+    ref,
+} from 'vue'
+
+const darkMode = ref(false)
+const navBar = ref(null)
+const navBarSticky = ref(false)
+const items = ref([
+    {
+        label: 'Home',
+        url: '/'
+    },
+    {
+        label: 'Meet me',
+        url: 'meet-me'
+    },
+    {
+        label: 'CV',
+        url: 'cv'
+    },
+    {
+        label: 'Portfolio',
+        url: 'portfolio'
+    },
+    {
+        label: 'Contact',
+        url: 'contact'
+    },
+])
+
+const logo = computed(() => darkMode.value ? LogoWhite : LogoBlack)
+
+const intersectionObserver = new IntersectionObserver(
+    ([ entry ]) => navBarSticky.value = entry.intersectionRatio < 1,
+    { threshold: [1] },
+)
+const mutationObserver = new MutationObserver(([ entry ]) => {
+    if (entry.attributeName === 'class') {
+        darkMode.value = entry.target.classList.contains('dark')
+    }
+})
+
+onMounted(() => {
+    darkMode.value = document.body.parentNode.classList.contains('dark')
+
+    intersectionObserver.observe(navBar.value)
+    mutationObserver.observe(document.body.parentNode, { attributes: true })
+})
+onBeforeUnmount(() => {
+    intersectionObserver.disconnect()
+    mutationObserver.disconnect()
+})
+</script>
