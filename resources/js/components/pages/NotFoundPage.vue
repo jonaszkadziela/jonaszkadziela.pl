48289f60f70fbd73d3f00ad1358ef015a6b71e27
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-12-19 21:09:55 +0100

    Add empty Not Found page

---
+++ resources/js/components/pages/NotFoundPage.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="container mx-auto">
+        <h1 class="text-2xl">
+            Not Found
+        </h1>
+    </section>
+</template>
--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,7 +1,9 @@
 const CvPage = () => import('../components/pages/CvPage.vue')
 const HomePage = () => import('../components/pages/HomePage.vue')
+const NotFoundPage = () => import('../components/pages/NotFoundPage.vue')
 
 export default [
     { path: '/', name: 'home', component: HomePage },
     { path: '/cv', name: 'cv', component: CvPage },
+    { path: '/:pathMatch(.*)*', name: 'error-404', component: NotFoundPage },
 ]
