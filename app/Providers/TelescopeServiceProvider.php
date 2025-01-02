2e530297bacbbaffc8b4adc89675a8a70f66016d
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-01-02 17:42:48 +0100

    Change TelescopeServiceProvider configuration

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
@@ -14,28 +15,30 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     public function register(): void
     {
-        // Telescope::night();
-
-        $this->hideSensitiveRequestDetails();
-
         $isLocal = $this->app->environment('local');
 
-        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
-            return $isLocal ||
-                   $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
-        });
+        $this->hideSensitiveRequestDetails($isLocal);
+
+        Telescope::filter(
+            fn (IncomingEntry $entry) =>
+                config('telescope.filtering_enabled') === false ||
+                $isLocal ||
+                $entry->isReportableException() ||
+                $entry->isFailedRequest() ||
+                $entry->isFailedJob() ||
+                $entry->isScheduledTask() ||
+                $entry->hasMonitoredTag()
+        );
+
+        Telescope::night();
     }
 
     /**
      * Prevent sensitive request details from being logged by Telescope.
      */
-    protected function hideSensitiveRequestDetails(): void
+    protected function hideSensitiveRequestDetails(bool $isLocal): void
     {
-        if ($this->app->environment('local')) {
+        if ($isLocal === true) {
             return;
         }
 
@@ -55,10 +58,6 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
-        });
+        Gate::define('viewTelescope', fn (User $user) => $user->is_admin === true);
     }
 }
