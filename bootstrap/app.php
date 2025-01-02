9c2416be1cf67a3f428004af9b5a83787953c9ea
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-01-02 17:42:55 +0100

    Schedule telescope:prune command to run daily

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\SetLanguage;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('telescope:prune --hours=168')->daily();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api([
             SetLanguage::class,
