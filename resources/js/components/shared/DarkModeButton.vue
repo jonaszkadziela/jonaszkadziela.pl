0e10a50d90b1e26d83b51529f35f045f35dc6213
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-01-04 22:54:52 +0100

    Fix dark mode handling when APP_DEFAULT_THEME is empty

--- resources/js/components/shared/DarkModeButton.vue
+++ resources/js/components/shared/DarkModeButton.vue
@@ -6,13 +6,25 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import {
+    computed,
+    onMounted,
+} from 'vue'
 
 const props = defineProps(['darkMode'])
-
 const darkModeIcon = computed(() => props.darkMode ? 'fa fa-moon' : 'fa fa-sun')
+const html = document.body.parentNode
 
 function toggleDarkMode() {
-    return props.darkMode ? document.body.parentNode.classList.replace('dark', 'light') : document.body.parentNode.classList.replace('light', 'dark')
+    return props.darkMode ? html.classList.replace('dark', 'light') : html.classList.replace('light', 'dark')
 }
+
+onMounted(() => {
+    if (html.classList.contains('light') || html.classList.contains('dark')) {
+        return
+    }
+
+    html.classList.toggle('dark', window.matchMedia('(prefers-color-scheme: dark)').matches)
+    html.classList.toggle('light', window.matchMedia('(prefers-color-scheme: light)').matches)
+})
 </script>
