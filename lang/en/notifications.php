7f692d05e0d2e5d853dec33f1ca3b97e83e2596a
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-01-25 16:33:26 +0100

    Add Notifications translations

---
+++ lang/en/notifications.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in notifications.
+    |
+    */
+
+    'action-button-help' => 'If you\'re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser',
+    'all-rights-reserved' => 'All rights reserved',
+    'greeting' => 'Hello!',
+    'regards' => 'Regards',
+    'signature' => ':app Admin',
+    'whoops' => 'Whoops!',
+
+];
---
+++ lang/pl/notifications.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in notifications.
+    |
+    */
+
+    'action-button-help' => 'Jeśli masz problemy z kliknięciem przycisku ":actionText", skopiuj i wklej poniższy adres URL do swojej przeglądarki internetowej',
+    'all-rights-reserved' => 'Wszelkie prawa zastrzeżone',
+    'greeting' => 'Cześć!',
+    'regards' => 'Pozdrawiam',
+    'signature' => 'Administrator :app',
+    'whoops' => 'Upsss!',
+
+];
