dd7b0cd89f19069e50fdcbf344fc6170db3c4617
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-01-26 18:08:01 +0100

    Replace hardcoded socials in App with real data

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -24,44 +24,7 @@ const toast = useToast()
 
 const darkMode = ref(false)
 const menuData = ref([])
-const socialData = [
-    {
-        id: 1,
-        title: 'Facebook',
-        link: 'https://facebook.com/jonaszkadzielapl',
-        icon: 'fa-brands fa-facebook-f',
-    },
-    {
-        id: 2,
-        title: 'LinkedIn',
-        link: 'https://linkedin.com/in/jonaszkadziela',
-        icon: 'fa-brands fa-linkedin-in',
-    },
-    {
-        id: 3,
-        title: 'X (Twitter)',
-        link: 'https://x.com/jonaszkadziela',
-        icon: 'fa-brands fa-x-twitter',
-    },
-    {
-        id: 4,
-        title: 'YouTube',
-        link: 'https://youtube.com/jonaszkadziela',
-        icon: 'fa-brands fa-youtube',
-    },
-    {
-        id: 5,
-        title: 'GitHub',
-        link: 'https://github.com/jonaszkadziela',
-        icon: 'fa-brands fa-github',
-    },
-    {
-        id: 6,
-        title: 'Stack Overflow',
-        link: 'https://stackoverflow.com/users/9310738/jonasz-kadziela',
-        icon: 'fa-brands fa-stack-overflow',
-    },
-]
+const socialData = ref([])
 
 const mutationObserver = new MutationObserver(([ entry ]) => {
     if (entry.attributeName === 'class') {
@@ -87,6 +50,15 @@ onMounted(() => {
                 }
             })
         })
+        .catch(() => toast.add({
+            severity: 'error',
+            summary: Lang.get('toast.error.load-data.summary'),
+            detail: Lang.get('toast.error.load-data.detail'),
+        }))
+
+    axios
+        .get('/socials')
+        .then(response => socialData.value = response.data)
         .catch(() => toast.add({
             severity: 'error',
             summary: Lang.get('toast.error.load-data.summary'),
