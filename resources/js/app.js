d1286e2619802d7903291dca754e99c1ea01f296
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-02-01 23:41:46 +0100

    Add @fortawesome/fontawesome-free package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap'
+import '@fortawesome/fontawesome-free/css/all.css'
 import { createApp } from 'vue'
 import { definePreset } from '@primevue/themes'
 import { getTranslation, hasTranslation } from './translation.js'
