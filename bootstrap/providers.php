f74b798d7a2b66acf4ca8d7de9d6ac097f06c11a
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-02-16 21:29:29 +0100

    Add filament configuration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\RouteServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     Torann\GeoIP\GeoIPServiceProvider::class,
