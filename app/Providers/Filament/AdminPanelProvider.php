f0cd080481dcf7b2a0f3130b4ee6fac8fb3c3362
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-02-17 00:08:31 +0100

    Add basic language switcher to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers\Filament;
 
+use App\Http\Middleware\SetLanguage;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -18,6 +20,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
@@ -38,6 +41,12 @@ class AdminPanelProvider extends PanelProvider
             ->darkModeBrandLogo(Vite::asset('resources/images/brand/logo-white.svg'))
             ->brandLogo(Vite::asset('resources/images/brand/logo-black.svg'))
             ->brandLogoHeight('48px')
+            ->userMenuItems([
+                MenuItem::make()
+                    ->label(fn () => Lang::get('admin.change-language'))
+                    ->url(fn () => Lang::getLocale() === 'en' ? '/language/pl' : '/language/en')
+                    ->icon('heroicon-s-globe-europe-africa'),
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -54,6 +63,7 @@ class AdminPanelProvider extends PanelProvider
                 StartSession::class,
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
+                SetLanguage::class,
                 VerifyCsrfToken::class,
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
---
+++ lang/pl/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the Admin panel.
+    |
+    */
+
+    'change-language' => 'Zmień na angielski',
+
+];
