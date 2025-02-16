d46896191d7a9ea43b259c7161866c05d8921a71
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-02-16 22:43:39 +0100

    Add hot reloading for filament resources

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import Components from 'unplugin-vue-components/vite'
-import laravel from 'laravel-vite-plugin'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
@@ -17,7 +17,10 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Filament/**',
+            ],
         }),
         vue(),
         Components({
