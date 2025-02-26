e2d714544011e0a87330bca6acae324231b02ba0
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-02-26 19:35:32 +0100

    Change default APP_URL to use HTTPS

--- tests/Feature/Api/FeedbackTest.php
+++ tests/Feature/Api/FeedbackTest.php
@@ -18,7 +18,7 @@ class FeedbackTest extends TestCase
         'body' => 'Error 404 - JonaszKadziela.pl',
         'data' => [
             '_telescope' => 1739642093167,
-            'url' => 'http://jonaszkadziela.test/error',
+            'url' => 'https://jonaszkadziela.test/error',
             'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
         ],
     ];
