0bda23fa6ae28c93adfca69c2374f129ae7e459e
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-03-15 21:18:49 +0100

    Add caching to file show route

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/language/{code}', [UserSettingController::class, 'language']);
 Route::post('/theme/{mode}', [UserSettingController::class, 'theme']);
 
-Route::get('/files/{file:slug}', [FileController::class, 'show']);
+Route::get('/files/{file:slug}', [FileController::class, 'show'])->middleware('cache.headers:public;max_age=2628000;etag');
 Route::get('/users/current', [UserController::class, 'currentUser']);
 
 Route::get('{any}', fn () => view('vue'))->where('any', '.*');
