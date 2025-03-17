5e9b7547eaca34c47cd960527498aa62791da71c
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-03-17 21:10:10 +0100

    Change text color in HeaderSection and FooterSection when dark mode is enabled

--- resources/js/components/sections/FooterSection.vue
+++ resources/js/components/sections/FooterSection.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="cv-width mx-auto px-12 py-4">
-        <p class="dark:text-black justify text-sm text-white">
+        <p class="dark:text-gray-300 justify text-sm text-white">
             {{ getTranslation(translations, section.body) }}
         </p>
     </div>
