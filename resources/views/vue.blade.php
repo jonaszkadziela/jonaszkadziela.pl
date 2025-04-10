956befd06d813a92d2f838bc4e3a45504086ee12
Jonasz Kądziela <contact@jonaszkadziela.pl>
2025-04-10 20:13:21 +0200

    Add versioning to messages.js file

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -1,5 +1,5 @@
 <x-main-layout with-analytics>
-    <script src="{{ asset('/build/messages.js') }}"></script>
+    <script src="{{ asset('/build/messages.js?v=' . Vite::manifestHash()) }}"></script>
     <script nonce="{{ csp_nonce() }}">
         Lang.setLocale('{{ App::getLocale() }}')
     </script>
